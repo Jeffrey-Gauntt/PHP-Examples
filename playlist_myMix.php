c899cde9c7d5de821c1555f7beed729bcccbd3b8
Jeffrey Gauntt <92536471+Jeffrey-Gauntt@users.noreply.github.com>
2022-09-23 21:33:14 -0500

    initial commit

--- playlist_myMix.php
+++ playlist_myMix.php
@@ -9,13 +9,13 @@ $userId = $_POST['userId'];
 $playlistName = $_POST['playlistName'];
 
 //get playlist Id
-$query = mysqli_query($con, "SELECT playlist_id FROM playlists WHERE name = '$playlistName' AND user_id = '$userId'");
+$query = mysqli_query($con, "SELECT playlist_id FROM song_playlists WHERE name = '$playlistName' AND id = '$userId'");
 $array = mysqli_fetch_array($query);
 $uniqueArray = array_unique($array);
 $playlistId = implode($uniqueArray);
 
 //get track id's from playlist onto an array in proper order
-$query = mysqli_query($con, "SELECT * FROM playlist_tracks WHERE playlist_id = '$playlistId'");
+$query = mysqli_query($con, "SELECT * FROM song_playlist_tracks WHERE song_playlist_id = '$playlistId'");
 if($query->num_rows > 0) {
     $playlistTrackIds = array();
     while($row = mysqli_fetch_array($query)) {
@@ -23,7 +23,7 @@ if($query->num_rows > 0) {
     }
     $playlist = implode("', '", $playlistTrackIds);
     //query tracks to results list
-    $query = mysqli_query($con, "SELECT * FROM user_tracks WHERE statuss = 'Active' AND perm = 'yes' AND track_id IN ('" . $playlist . "') ORDER BY RAND()");
+    $query = mysqli_query($con, "SELECT * FROM songs WHERE status = '1' AND perm = '1' AND song_id IN ('" . $playlist . "') ORDER BY RAND()");
     if($query->num_rows > 0) {
         $currentPlaylist = array();
         while($row = mysqli_fetch_array($query)) {
@@ -32,41 +32,41 @@ if($query->num_rows > 0) {
 
 			// get shareable profile link
 			$userShareId = $row['user_id'];
-			$shareableProfileLink = mysqli_query($con, "SELECT * FROM users WHERE user_id = '$userShareId'");
+			$shareableProfileLink = mysqli_query($con, "SELECT * FROM members WHERE id = '$userShareId'");
 			$shareableProfileLink = mysqli_fetch_assoc($shareableProfileLink);
-			$shareableProfileLink = $shareableProfileLink['shareable_link'];
+			$shareableProfileLink = $shareableProfileLink['link_share'];
 
             ?>
-            <section title="Click to play" class="resultitem" onclick="setTrack('<?= $row['track_id']; ?>', newPlaylist, true)">
+            <section title="Click to play" class="resultitem" onclick="setTrack('<?= $row['id']; ?>', newPlaylist, true)">
                 <article class="resultimgcontainer">
                     <img src="<?= $row['picture_path']; ?>" alt="User Picture" class="resultimg">
                 </article>
                 <article class="resultinfo">
                     <div class="resultinfotop">
                         <div class="resultinfoleft">
-                            <p class="resulttrackname"><?= $row['track_title']; ?></p>
-                            <p class="resultusername"><?= $row['username']; ?></p>
+                            <p class="resulttrackname"><?= $row['title']; ?></p>
+                            <p class="resultusername"><?= $row['name']; ?></p>
                             <!-- <p class="resultuserregion"><?= $row['country']; ?></p> -->
-                            <p class="resultuserregion regionSelect" name="regionSelect" value="<?= $row['region']; ?>"><?= $row['region']; ?></p>
+                            <p class="resultuserregion regionSelect" name="regionSelect" value="<?= $row['area']; ?>"><?= $row['area']; ?></p>
                         </div>
                         <div class="resultinforight">
                             <p class="resultplays">Plays: <span class="resultplaycount"><?= $row['plays']; ?></span></p>
                             <div id="likesdislikesmorecontainer">
                                 <?php
-                                $listElements = new PlaylistResultsListElementProvider($con, $userIdLoggedIn, $row['track_id'], $row['likes'], $row['dislikes'], $playlistId, $row['user_id'], $row['username'], $shareableProfileLink);
+                                $listElements = new PlaylistResultsListElementProvider($con, $userIdLoggedIn, $row['id'], $row['likes'], $row['dislikes'], $playlistId, $row['id'], $row['name'], $shareableProfileLink);
                                 echo $listElements->createPlaylistResultsListElements();        
                                 ?>  
                             </div>
                         </div>
                     </div>
-                    <p class="resultgenres"><?php echo $row['primary_genre'];
+                    <p class="resultgenres"><?php echo $row['primary'];
         
-                                                                    if($row['secondary_genre'] != "none") {
-                                                                        echo ", " . $row['secondary_genre'];
+                                                                    if($row['secondary'] != "none") {
+                                                                        echo ", " . $row['secondary'];
                                                                     }
 
-                                                                                    if($row['tertiary_genre'] != "none") {
-                                                                                        echo ", " . $row['tertiary_genre'];
+                                                                                    if($row['tertiary'] != "none") {
+                                                                                        echo ", " . $row['tertiary'];
                                                                                     }
                                             ?>
                     </p>
@@ -91,8 +91,8 @@ if($query->num_rows > 0) {
 ?>
 
 <script>
-    var newPlaylist = <?php echo $currentPlaylistJson; ?>;
-    // console.log("My Mix, " + newPlaylist);
+    var newList = <?php echo $currentPlaylistJson; ?>;
+    // console.log("My Mix, " + newList);
 </script>
 
 
